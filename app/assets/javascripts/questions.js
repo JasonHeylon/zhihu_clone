ec397f36bc8d8855fc026bb2ea0031320a6f01e7
Jason Heylon <jasonheylon@gmail.com>
2015-01-16 00:11:32 +0800

    add editor for new answer

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+var question_show_page = function(){
+  if ($('body').data('controller') == "questions" && $('body').data('action') == "show") {
+    var editor = new Simditor({
+      textarea: $('#txtNewAnswer')
+    });
+
+  }
+};
+
+$(document).ready(question_show_page);
+$(document).on('page:load', question_show_page);
